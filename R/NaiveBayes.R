d2783c6bc74ddf88aab38a07a4fafc9e0a69c1b8
Adele <anry_1225@yahoo.co.jp>
2019-05-22 12:24:59 +0300

    Update NaiveBayes.R

--- R/NaiveBayes.R
+++ R/NaiveBayes.R
@@ -1,2 +1,17 @@
 # I would like to consider about the imputation using a Naive Bayes.
 #
+
+
+#
+# preperations
+options(warn=-1) # turning "warnings" off
+default_par = par() # save default par
+# pararell computing options
+install.packages("doParallel"); library(doParallel)
+cl <- makePSOCKcluster(4); registerDoParallel(cl)
+
+
+# install the package caret
+install.packages("caret"); library("caret")
+# for Naive Bayes
+library("klaR")
